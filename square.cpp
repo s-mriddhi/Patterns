d8de8e978d0aea0d64daa1953a93abaa8e37eb8f
s-mriddhi <samsris06@gmail.com>
2025-06-28 11:23:33 +0530

    closest edge distance problem p22

--- square.cpp
+++ square.cpp
@@ -8,7 +8,18 @@ void print1(int n){
         cout<< endl;
     }
 }
-
+void print22(int n) {
+    for(int i=0;i<2*n-1;i++){//god level logic
+        for (int j=0;j<2*n-1;j++){
+           int top=i;
+           int left=j;
+           int bottom= (2*n-2)-i;
+           int right= 2*n-2-j;
+           cout<< n- min(min(right,left), min(top,bottom));
+        }
+        cout<< endl;
+    }
+}
 int main(){
 int n;
 cin>> n;
